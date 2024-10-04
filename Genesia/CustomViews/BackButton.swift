c9cd7e0045e3c4c447e5cb164f8380feb1da796d
GehadP <gehadgamal045@gmail.com>
2024-10-04 20:16:24 +0300

    update chat view

--- Genesia/CustomViews/BackButton.swift
+++ Genesia/CustomViews/BackButton.swift
@@ -31,3 +31,22 @@ struct CustomBackButton: View {
   CustomBackButton()
 }
 
+struct CustomXButton: View {
+  var body: some View {
+    ZStack {
+      Image(systemName: "xmark")
+        .frame(width: 35, height: 35)
+        .foregroundStyle(.darkBlue)
+        .font(.system(size: 10))
+        .bold()
+        .background(.ultraThinMaterial)
+        .clipShape(Circle())
+    }
+  }
+}
+
+#Preview {
+  CustomXButton()
+}
+
+
--- Genesia/Flow/Chat Screen/ChatScrenView.swift
+++ Genesia/Flow/Chat Screen/ChatScrenView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 enum ChatScreenViewPushedFrom:Hashable {
-  case chatList
-  case personalitySelection
+    case chatList
+    case personalitySelection
 }
 struct ChatScreenView: View {
     @EnvironmentObject var vm:LandingFlowViewModel
@@ -18,12 +18,14 @@ struct ChatScreenView: View {
     @State var showProScreen:Bool = false
     
     var body: some View {
-        ZStack {
+        ZStack{
+            Color.darkBlue
+            .ignoresSafeArea()
             VStack {
-                Spacer()
                 
-                // Message input area
-                HStack {
+                headerView
+                Spacer()
+                HStack{
                     TextField("Type your message...", text: .constant(""))
                         .padding()
                         .background(Color.gray.opacity(0.2))
@@ -39,155 +41,143 @@ struct ChatScreenView: View {
                             .foregroundColor(.gray)
                     }
                 }
-                .padding()
-                .background(Color.darkBlue)
-            }.navigationBarBackButtonHidden()
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        CustomBackButton()
-                    }
-                    ToolbarItem(placement: .principal) {
-                        HStack {
-                            Image(vm.userChoices.selectedAvatar ?? "person1")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .clipShape(Circle())
-                            
-                            VStack(alignment: .leading) {
-                                Text(vm.userChoices.aiName)
-                                    .foregroundColor(.white)
-                                    .font(.headline)
-                                HStack {
-                                    Circle()
-                                        .fill(Color.green)
-                                        .frame(width: 8, height: 8)
-                                    
-                                    Text("Tap for more info")
-                                        .foregroundColor(.gray)
-                                        .font(.caption)
-                                }
-                            }
-                        }
-                    }
-                    ToolbarItem(placement: .topBarTrailing) {
-                        HStack{
-                            Button(action: {
-                                
-                            }) {
-                                Text("PRO")
-                                    .font(.headline)
-                                    .padding(.horizontal, 14)
-                                    .padding(.vertical, 6)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 16)
-                                            .fill(Color.gray.opacity(0.3))
-                                    )
-                                    .foregroundColor(.white)
-                            }
-                            Button(action: {
-                            }) {
-                                Image(systemName: "video.fill")
-                                    .foregroundColor(.white)
-                                    .padding(.horizontal, 12)
-                                    .padding(.vertical, 6)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 16)
-                                            .fill(Color.clear)
-                                    )
-                            }
-                            
-                        }
-                        
-                    }
-                    
-                }
-            
-                .background(Color.darkBlue.ignoresSafeArea())
+            }
+
+                //.background(Color.darkBlue.ignoresSafeArea())
             if showSetRelationShipView {
                 relationShipView
+                    .transition(.move(edge: .bottom))
+                    .transition(.opacity)
+                    .zIndex(1)
             }
-        
         }
+        .navigationBarBackButtonHidden()
     }
 }
 extension ChatScreenView {
-    private var relationShipView: some View {
-      ZStack {
-        BlurBackground(style: .dark)
-          .ignoresSafeArea()
-          .onTapGesture {
-            withAnimation {
-              showSetRelationShipView = false
-            }
+
+    private var headerView:some View {
+      HStack {
+        CustomBackButton()
+        Spacer()
+          Image(vm.userChoices.selectedAvatar )
+          .resizable()
+          .frame(width: 50,height: 50)
+          .clipShape(Circle())
+        VStack(alignment:.leading) {
+          HStack{
+              Text(vm.userChoices.aiName)
+            Text("·")
+              .font(.system(size: 30))
+              .bold()
+              .foregroundStyle(.green)
           }
-        VStack {
-          Spacer()
-          RoundedTopCorners(radius: 30)
-            .fill(Color.darkBlue)
-            .frame(height:450)
-            .overlay {
-              VStack {
-                  CustomBackButton()
-                  .offset(x:-150)
-                  .onTapGesture {
-                    withAnimation {
-                      showSetRelationShipView = false
-                    }
-                  }
-                ZStack {
-                  Circle()
-                    .fill(Color.white)
-                    .frame(width: 105, height: 105) // Adjust the size to create the desired border thickness
-                  
-                    Image(vm.userChoices.selectedAvatar ?? "")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
-                }
-                Text("Set Your Relatioship with\n \(vm.userChoices.aiName)")
-                  .font(.system(size: 25))
-                  .fontWeight(.semibold)
-                  .foregroundStyle(.white)
-                  .multilineTextAlignment(.center)
-                  .padding()
-                Text("Friends")
-                  .foregroundStyle(.white)
-                  .font(.system(size: 20))
-                  .fontWeight(.semibold)
-                  .frame(width: 300, height: 60, alignment: .center)
-                  .background(RoundedRectangle(cornerRadius: 30)
-                    .stroke(Color.white))
-                  .overlay {
-                    RoundedRectangle(cornerRadius: 30)
-                      .fill(Color.white.opacity(0.4))
-                  }
-                  .onTapGesture {
-                    withAnimation {
-                      showSetRelationShipView = false
-                    }
-                  }
-                RoundedRectangle(cornerRadius: 30)
-                  .frame(width:300,height:60)
-                  .foregroundStyle(.white.opacity(0.1))
-                  .padding(5)
-                  .overlay{
-                    HStack {
-                      Image(systemName: "lock.fill")
-                      Text("Romantic Partners")
-                        .font(.system(size: 20))
-                        .fontWeight(.semibold)
-                    }.foregroundStyle(.white)
-                  }
-                  .onTapGesture {
-                    showProScreen = true
-                  }
-              }
+          Text("Tap for more info")
+            .font(.footnote)
+            .foregroundStyle(.gray)
+        }.foregroundStyle(.white)
+        Spacer()
+        HStack {
+          HStack {
+            Text("PRO")
+              .foregroundStyle(.white)
+              .bold()
+          }.shadow(color: .white, radius: 10, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 5)
+            .frame(width: 50)
+            .padding(10)
+            .background(
+              RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.white)
+            )
+            .onTapGesture {
+              showProScreen = true
             }
-        }.ignoresSafeArea()
+          Image(systemName: "video.fill")
+            .foregroundStyle(.gray)
+            .font(.system(size: 22))
+            .padding(.leading,10)
+        }
       }
+      .padding()
     }
 
+    private var relationShipView: some View {
+        ZStack {
+            BlurBackground(style: .dark)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    withAnimation {
+                        showSetRelationShipView = false
+                    }
+                }
+            VStack {
+                Spacer()
+                RoundedTopCorners(radius: 30)
+                    .fill(Color.darkBlue)
+                    .frame(height:450)
+                    .overlay {
+                        VStack {
+                            CustomXButton()
+                                .offset(x:-150)
+                                .onTapGesture {
+                                    withAnimation {
+                                        showSetRelationShipView = false
+                                    }
+                                }
+                            ZStack {
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 105, height: 105) // Adjust the size to create the desired border thickness
+                                
+                                Image(vm.userChoices.selectedAvatar )
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(Circle())
+                            }
+                            Text("Set Your Relatioship with\n \(vm.userChoices.aiName)")
+                                .font(.system(size: 25))
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                            Text("Friends")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 20))
+                                .fontWeight(.semibold)
+                                .frame(width: 300, height: 60, alignment: .center)
+                                .background(RoundedRectangle(cornerRadius: 30)
+                                    .stroke(Color.white))
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .fill(Color.white.opacity(0.4))
+                                }
+                                .onTapGesture {
+                                    withAnimation {
+                                        showSetRelationShipView = false
+                                    }
+                                }
+                            RoundedRectangle(cornerRadius: 30)
+                                .frame(width:300,height:60)
+                                .foregroundStyle(.white.opacity(0.1))
+                                .padding(5)
+                                .overlay{
+                                    HStack {
+                                        Image(systemName: "lock.fill")
+                                        Text("Romantic Partners")
+                                            .font(.system(size: 20))
+                                            .fontWeight(.semibold)
+                                    }.foregroundStyle(.white)
+                                }
+                                .onTapGesture {
+                                    showProScreen = true
+                                }
+                        }
+                    }
+            }.ignoresSafeArea()
+        }
+    }
+    
 }
 struct ChatScreenView_Previews: PreviewProvider {
     static var previews: some View {
@@ -199,25 +189,25 @@ struct ChatScreenView_Previews: PreviewProvider {
 
 
 struct RoundedTopCorners: Shape {
-  var radius: CGFloat = .infinity
-  
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-    path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + radius))
-    path.addArc(center: CGPoint(x: rect.minX + radius, y: rect.minY + radius),
-                radius: radius,
-                startAngle: .degrees(180),
-                endAngle: .degrees(270),
-                clockwise: false)
-    path.addLine(to: CGPoint(x: rect.maxX - radius, y: rect.minY))
-    path.addArc(center: CGPoint(x: rect.maxX - radius, y: rect.minY + radius),
-                radius: radius,
-                startAngle: .degrees(270),
-                endAngle: .degrees(0),
-                clockwise: false)
-    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-    path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-    return path
-  }
+    var radius: CGFloat = .infinity
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + radius))
+        path.addArc(center: CGPoint(x: rect.minX + radius, y: rect.minY + radius),
+                    radius: radius,
+                    startAngle: .degrees(180),
+                    endAngle: .degrees(270),
+                    clockwise: false)
+        path.addLine(to: CGPoint(x: rect.maxX - radius, y: rect.minY))
+        path.addArc(center: CGPoint(x: rect.maxX - radius, y: rect.minY + radius),
+                    radius: radius,
+                    startAngle: .degrees(270),
+                    endAngle: .degrees(0),
+                    clockwise: false)
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        return path
+    }
 }
